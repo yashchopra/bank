085d8e645dbc46e2f82c10fcb429f66b064ba90f
yashchopra <ychopra@asu.edu>
2017-11-02 00:20:49 -0700

    uncommented validations and added ssn uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,25 +10,25 @@ class User < ApplicationRecord
 	enum isEligibleForTier1: [:yes, :no]
 
   encrypt :ssn, searchable: true, hash_salt: ENV["SECRET_KEY_BASE"], key: ENV["SECRET_KEY_BASE"]
-  #
-	# validates_presence_of :first_name
-	# validates_presence_of :last_name
-	# validates_presence_of :city
-	# validates_presence_of :state
-	# validates_presence_of :country
-  #
-	# validates :zip, :presence => true,
-	# 					      :numericality => true,
-	# 							  :length => {:minimum => 1}
-  #
-	# validates :phone,:presence => true,
-	# 					:numericality => true,
-	# 					:length => { :minimum => 10, :maximum => 10 }
 
-	# validates :ssn,:presence => true,
-	#  					:numericality => true,
-	#  					:length => { :minimum => 9, :maximum => 9 }
+	validates_presence_of :first_name
+	validates_presence_of :last_name
+	validates_presence_of :city
+	validates_presence_of :state
+	validates_presence_of :country
 
+	validates :zip, :presence => true,
+						      :numericality => true,
+								  :length => {:minimum => 1}
+
+	validates :phone,:presence => true,
+						:numericality => true,
+						:length => { :minimum => 10, :maximum => 10 }
+
+	validates :ssn,:presence => true,
+						:numericality => true,
+						:length => { :minimum => 9, :maximum => 9 },
+						:uniqueness => true
 
 	def set_default_role
 		self.role ||= :customer
