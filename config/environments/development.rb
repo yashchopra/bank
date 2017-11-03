c207b0189d861b18ca4eb5ad6e2d72d735316cbb
yashchopra <ychopra@asu.edu>
2017-11-02 18:36:42 -0700

    force SSL

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,7 @@ Rails.application.configure do
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.perform_deliveries = true
+  config.force_ssl = true
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
