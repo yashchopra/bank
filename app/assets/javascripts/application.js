48de330e3f8b5e5e65e6806a0eb9b3a1170bc800
Krishna Nadoli <knadoli@gmail.com>
2017-11-02 19:07:40 -0700

    Since the lack of turbolinks in application.js was causing yall issues, i added jquery-turbolinks to the file, and hopefully this should squash all the issues with turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,4 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
+ ../.// This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
@@ -11,6 +11,7 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery-turbolinks
 //= require materialize-sprockets
 //= require rails-ujs
 //= require keyboard_application
